38e8fceef2f97a0deb40096b575066703cb510a2
tomek makuch <tomek.makuch@gmail.com>
2022-03-08 09:57:04 +0100

    lesson 1 : add comments : 2

--- 1_assert.py
+++ 1_assert.py
@@ -3,3 +3,8 @@ def apply_sth(some_number):
 	print(f'{some_number}')
 
 apply_sth(-1)
+
+
+# Things to remember:
+# Do not use assert for data validation
+# - assert can be disbaled with global debug flag thefore it's not a good idea to use it like so.
