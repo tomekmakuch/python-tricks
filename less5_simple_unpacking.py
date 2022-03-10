f6d5dadccd145d3482cf81bc1b3b8f79cf0fa8be
tomek makuch <tomek.makuch@gmail.com>
2022-03-10 15:22:59 +0100

    lesson 5 : simple unpacking

---
+++ less5_simple_unpacking.py
@@ -0,0 +1,9 @@
+some_var = ('a', 'b', 'c', 'd', 'e')
+
+some_a, some_b, some_c, some_d, _ = some_var
+
+print('some_a: ' + some_a)
+print('some_b: ' + some_b)
+print('some_c: ' + some_c)
+print('some_d: ' + some_d)
+print('_: ' + _)
