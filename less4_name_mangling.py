c77a71161106c45d446295576273ddd96d63876b
tomek makuch <tomek.makuch@gmail.com>
2022-03-10 15:19:17 +0100

    Fix namings : 2

--- less4_name_mangling.py
+++ less4_name_mangling.py
@@ -4,15 +4,36 @@ class Test:
 		self._b = 12
 		self.__c = 13
 
-t = Test()
-print(dir(t))
 
-
-class ExtendedTest:
+class ExtendedTest(Test):
 	def __init__(self):
 		super().__init__()
 		self.a = 'modified'
 		self._b = 'modified'
 		self.__c = 'modified'
 
-import ipdb; ipdb.set_trace()
+	def get_mangled(self):
+		return self.__c
+
+
+_ManagledGlobal__mangled = 23
+
+class ManagledGlobal:
+	def test(self):
+		return __mangled
+
+print(ManagledGlobal().test())
+
+# Dunder methods ("double underscore")
+
+
+class SomeClass:
+
+	# example of a dunder method
+	def __init__(self):
+		pass
+
+	# example of a dunder method
+	def __call__(self):
+		pass
+
