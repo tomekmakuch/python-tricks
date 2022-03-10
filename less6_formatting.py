0b909a6c453cb55c6958557353a91e414cb061cb
tomek makuch <tomek.makuch@gmail.com>
2022-03-10 15:32:28 +0100

    Lesson 6 : formatting strings

---
+++ less6_formatting.py
@@ -0,0 +1,14 @@
+# Old style formatting
+print('Old style : Hello, %s' % 'you!')
+
+# Old style formatting - multiple vars
+print('Old style : Hello, %s and %s' % ('you', 'you!'))
+
+# Newer formatting style
+
+print('Newer style : Hello, {}'.format('you!'))
+
+# Newest : Formatted String Literals
+
+name =  'you'
+print(f'Formatted String Literals : Hello, {name}')
